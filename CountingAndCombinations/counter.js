f03a2733e769d791d1b6aecb8dd3eac5476e2d4d
Andrew Ribeiro <andrewnetwork@gmail.com>
2018-07-12 14:01:01 -0400

    Added multichoice support

--- CountingAndCombinations/counter.js
+++ CountingAndCombinations/counter.js
@@ -66,25 +66,77 @@ function countingMatrix(digits,base){
 function goHandler(){
     const n = parseInt(d3.select("#n").property("value"));
     const k = parseInt(d3.select("#k").property("value"));
+    const b = parseInt(d3.select("#b").property("value"));
+    const visSel = d3.select("#visSel").property("value");
 
-    renderFormula(n,k);
+    renderFormula(n,k,b);
 
     d3.select("#countTable").selectAll("*").remove();
     d3.select("#countTable").append("div").attr("id","outBar");
     
+    if(visSel == "ct"){
+        setTimeout(()=>makeCountTables(n,b),0);
+    }else if(visSel == "kbc"){
+        setTimeout(()=>countInBase(n,k),0);
+    }
+}
+
+function binomialCoef(n,k){
+    return Math.floor(fact(n)/(fact(k)*fact(n-k)));
+}
+
+function createAxisTable(n,k,title){
+    const axisTable = d3.select("#countTable")
+                        .append("table");
+
+    axisTable.selectAll("tr")
+             .data(range(0,k**n))
+             .enter()
+             .append("tr")
+             .append("td")
+             .text((x)=>x);
+
+    axisTable.insert("tr","tr").append("th").text(title);
+
+    return axisTable;
+}
+function countInBase(n,k){
+    createAxisTable(n,k,"k = ");
+    const table = d3.select("#countTable").append("table");
+
+    if( !isNaN(n) && !isNaN(k) ){
+        const cm = countingMatrix(n,k);
+        table
+        .selectAll("tr")
+        .data(cm).enter()
+        .append("tr")
+        .selectAll("td")
+        .data((x)=>x).enter()
+        .append("td").text((x)=>x)
+
+        table
+        .insert("tr","tr")
+        .append("th")
+            .attr("colspan",n)
+            .text(k);
+    }
+}
+function makeCountTables(n,b){
+    createAxisTable(n,b,"k = ");
+
     for(var i = 0; i <= n; i++){
-        countTable(n,i);
+        countTable(n,i,b);
     }
 }
 function resetHandler(){
     renderFormula(NaN,NaN);
 }
 
-function countTable(n,k){
+function countTable(n,k,b){
     const table = d3.select("#countTable").append("table");
 
-    if( !isNaN(n) && !isNaN(k) ){
-        const cm = countingMatrix(n,2);
+    if( !isNaN(n) && !isNaN(k) && !isNaN(b) ){
+        const cm = countingMatrix(n,b);
         table
         .selectAll("tr")
         .data(cm).enter()
@@ -103,17 +155,27 @@ function countTable(n,k){
 
 }
 
-function renderFormula(n,k){
+function renderFormula(n,k,b){
     var math = MathJax.Hub.getAllJax("MathDiv")[0];
     var templateStr = null;
     console.log(n);
-    if( isNaN(n) || isNaN(k) ){
+    if( isNaN(n) || isNaN(k) || isNaN(b)){
         templateStr = "\\LARGE\\text{n choose k} = \\binom{n}{k} = \\frac{n!}{k!(n - k)!} \\tag{Binomial coefficient}";
     }
     else{
-        const res = Math.floor(fact(n)/(fact(k)*fact(n-k)));
-        templateStr = "\\LARGE \\text{"+n+" choose "+k+"}= \\binom{"+n+"}{"+k+"} = \\frac{"+n+"!}{"+k+"!("+n+"-"+k+")!} = "+
-            res+"\\tag{Binomial coefficient}";
+        if(b <= 2){
+            const res = binomialCoef(n,k);
+            templateStr = "\\LARGE \\text{"+n+" choose "+k+"}= \\binom{"+n+"}{"+k+"} = \\frac{"+n+"!}{"+k+"!("+n+"-"+k+")!} = "+
+                res+"\\tag{Binomial coefficient}";
+        }else{
+            // Multi choice problem. 
+            const multiN = n+k-1
+            const res = binomialCoef(multiN,k);
+            
+            templateStr = "\\LARGE \\text{"+n+" multichoose "+k+"}= \\left(\\binom{"+n+"+"+k+"-1}{"+k+"}\\right) = \\binom{"+multiN+"}{"+k+"}"
+                + " = \\frac{"+multiN+"!}{"+k+"!("+multiN+"-"+k+")!} = "+res+"\\tag{Multichoose}";
+        }
+        
     }
     
     MathJax.Hub.Queue(["Text",math,templateStr]);
